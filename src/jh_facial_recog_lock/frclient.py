2a0a04dd4ff96ac5d1fd71a2b546740de385a93f
Joshua Rothe <joshrothe@gmail.com>
2023-04-22 18:26:08 -0600

    Update frclient.py

--- src/jh_facial_recog_lock/frclient.py
+++ src/jh_facial_recog_lock/frclient.py
@@ -68,6 +68,8 @@ class FaceRecognition:
         self.text_type = cv2.FONT_HERSHEY_SIMPLEX
         self.line_type = cv2.LINE_AA
         self.printed = True
+        self.unlock_counter = 0 # custom
+        self.alert_counter = 0  # custom
 
     def cosine_distance(self, a, b):
         if a.shape != b.shape:
@@ -96,31 +98,31 @@ class FaceRecognition:
 
         if name[1] == "UNKNOWN":
             self.create_db(results)
-            unlock_counter = 0                                                                          # custom
-            alert_counter = alert_counter + 1                                                           # custom
+            self.unlock_counter = 0                                                                          # custom
+            self.alert_counter = self.alert_counter + 1                                                           # custom
             print("debug, if UNKNOWN true") # debug, remove
         else:                                                                                           # custom
-            unlock_counter = unlock_counter + 1                                                         # custom
-            alert_counter = 0                                                                           # custom
+            self.unlock_counter = self.unlock_counter + 1                                                         # custom
+            self.alert_counter = 0                                                                           # custom
             print("debug, if UNKNOWN false")    # debug, remove
-        if unlock_counter == 5:                                                                         # custom
+        if self.unlock_counter == 5:                                                                         # custom
             print("Unlocking . . .")    # debug, remove                                                 # custom
             GPIO.output(RELAY_PIN, 1)                                                                   # custom
         else:                                                                                           # custom
             print("Locking . . .")    # debug, remove                                                   # custom
             GPIO.output(RELAY_PIN, 0)                                                                   # custom
-        if alert_counter == 5:                                                                          # custom
+        if self.alert_counter == 5:                                                                          # custom
             try:                                                                                        # custom
                 #self.current_datetime = str(datetime.now())                                             # custom
                 #self.msg_string = 'Unauthorized entry attempt detected at %s' % (self.current_datetime) # custom
                 #pushSocket.send_string(self.msg_string)                                                 # custom
-                alert_counter = 0                                                                       # custom
+                self.alert_counter = 0                                                                       # custom
             except:                                                                                     # custom
                 print("Alert message to server failed to send.")                                        # custom
             else:                                                                                       # custom
                 print("Alert message to server sent successfully.")                                     # custom
-        print("Alert counter: ", alert_counter)    # debug, remove                                      # custom
-        print("Unlock counter: ", alert_counter)   # debug, remove                                      # custom
+        print("Alert counter: ", self.alert_counter)    # debug, remove                                      # custom
+        print("Unlock counter: ", self.unlock_counter)   # debug, remove                                      # custom
         return name
 
     def read_db(self, databases_path):
