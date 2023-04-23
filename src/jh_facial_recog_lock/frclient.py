43ac0350d98630e13689f5dfd851ffefd727a362
Joshua Rothe <joshrothe@gmail.com>
2023-04-22 19:35:10 -0600

    Update frclient.py

--- src/jh_facial_recog_lock/frclient.py
+++ src/jh_facial_recog_lock/frclient.py
@@ -106,7 +106,7 @@ class FaceRecognition:
             print("Unlocking.")                                                                         # custom
             GPIO.output(RELAY_PIN, 1)                                                                   # custom
             self.unlock_counter = self.unlock_counter + 1                                               # custom
-        if self.unlock_counter == 17:                                                                   # custom
+        if self.unlock_counter == 25:                                                                   # custom
             print("Access complete, locking.")                                                          # custom
             GPIO.output(RELAY_PIN, 0)                                                                   # custom
             self.unlock_counter = 0                                                                     # custom
